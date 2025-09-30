61c5f62649f041bbfc40a17f1e76820e5c5cd0a9
Giacomo Leidi <goodoldpaul@autistici.org>
2025-09-30 12:54:29 +0200

    Adapt to upstreamed dependecies.

--- modules/bonfire/packages/bonfire-libraries.scm
+++ modules/bonfire/packages/bonfire-libraries.scm
@@ -1,7 +1,10 @@
 ;;; SPDX-License-Identifier: GPL-3.0-or-later
-;;; Copyright © 2024 Giacomo Leidi <goodoldpaul@autistici.org>
+;;; Copyright © 2024, 2025 Giacomo Leidi <goodoldpaul@autistici.org>
 
 (define-module (bonfire packages bonfire-libraries)
+  #:use-module (gnu packages elixir-databases)
+  #:use-module (gnu packages elixir-markup)
+  #:use-module (gnu packages elixir-web)
   #:use-module (gnu packages elixir-xyz)
   #:use-module (guix build-system mix)
   #:use-module (guix download)
@@ -10,14 +13,12 @@
   #:use-module ((guix licenses)
                 #:prefix license:)
   #:use-module (guix packages)
-  #:use-module (bonfire packages elixir-databases)
-  #:use-module (bonfire packages elixir-web)
-  #:use-module (bonfire packages elixir-xyz))
+  #:use-module (bonfire packages elixir-databases))
 
 (define-public bonfire-data-access-control.git
   (let ((version "0.4.0")
         (revision "0")
-        (commit "de2ca8f732499b4df8f5a9711f7b7bb38df76609"))
+        (commit "3823ddfb857548ba4b938be25fb14b12993ee677"))
     (package
       (name "bonfire-data-access-control")
       (version (git-version version revision commit))
@@ -29,7 +30,7 @@
                (commit commit)))
          (file-name (git-file-name name version))
          (sha256
-          (base32 "0pfyb6si4sbzg9f8psw5lgz0h7jl29limc48sjc82lxq53nv22fr"))))
+          (base32 "1v3jnf3nr6xcla8f95rai17nn3i79xl0bycjq51rdwmrz234wn4z"))))
       (build-system mix-build-system)
       (arguments
        ;; There are no tests.
@@ -47,7 +48,7 @@
 (define-public bonfire-data-identity.git
   (let ((version "0.4.0")
         (revision "1")
-        (commit "f85a23060dd0cc7452b4cb9e96e655db12f3d488"))
+        (commit "f329680b520d33ff094742e7665c355aa094482d"))
     (package
       (name "bonfire-data-identity")
       (version (git-version version revision commit))
@@ -59,7 +60,7 @@
                (commit commit)))
          (file-name (git-file-name name version))
          (sha256
-          (base32 "0a9gmm0cgagwqxd2p3lm1zmx290hryvwzz4vyqhls7ysj9apivi0"))))
+          (base32 "1glvb84y7zc01plv20ly5x7zszabyjf3iivmrh6nyvpkdwk1034k"))))
       (build-system mix-build-system)
       (arguments
        ;; There are no tests.
@@ -80,7 +81,7 @@
 (define-public bonfire-data-edges.git
   (let ((version "0.4.0")
         (revision "0")
-        (commit "224212f3eb98bb2a534b4241b92b30dc30d0580b"))
+        (commit "89a6f7b8bdacf726baf4ae53f6c73065e6fc5ff2"))
     (package
       (name "bonfire-data-edges")
       (version (git-version version revision commit))
@@ -92,7 +93,7 @@
                (commit commit)))
          (file-name (git-file-name name version))
          (sha256
-          (base32 "1vz7b8sjx03bcm4afbwarjpfmz8dl4pnqv95qjcwycm9gqvz13yz"))))
+          (base32 "0xmag8l5qvawlgq13zaaixw40y7z0bagq7s361573s0gqdcxgp6g"))))
       (build-system mix-build-system)
       (native-inputs (list elixir-mess.git))
       (propagated-inputs (list elixir-needle))
@@ -107,156 +108,130 @@
 (define-public bonfire-paginator.git
   (let ((version "1.0.4")
         (revision "0")
-        (commit "52f0c069a3d90613b9bfe11ae7dd15eb89b7504e"))
+        (commit "faa9909568c86b74838ef5d036fecb003ea77c1f"))
     (package
-    (name "bonfire-paginator")
-    (version (git-version version revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/bonfire-networks/paginator.git")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32 "0hk1l63bvhf0axzwbaid5bn7jp5aq64qfd0fvl3hvxrsdzdvj5mq"))))
-    (build-system mix-build-system)
-    (arguments
-     (list
-      ;; Tests require a live Postgres process
-      #:tests? #f
-      #:phases
-      #~(modify-phases %standard-phases
-          (add-after 'unpack 'override-mix-env
-            (lambda _
-              (symlink (string-append (getcwd) "/config/dev.exs")
-                       "config/prod.exs"))))))
-    (native-inputs
-     (list elixir-ex-machina elixir-inch-ex))
-    (propagated-inputs
-     (list elixir-ecto
-           elixir-ecto-sql
-           elixir-needle-uid.git
-           elixir-plug-crypto
-           elixir-untangle))
-    (synopsis
-     "Cursor-based pagination for Elixir Ecto")
-    (description
-     "This package implements cursor-based pagination for Elixir Ecto.")
-    (home-page "https://github.com/bonfire-networks/paginator")
-    (license license:expat))))
-
-(define-public elixir-ecto-sparkles
-  (package
-    (name "elixir-ecto-sparkles")
-    (version "0.2.1")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (hexpm-uri "ecto_sparkles" version))
-       (sha256
-        (base32 "1ry89mk103jpv73971lwx3vmn9n6zd3ycya1ac1pr3r53f4hmkb0"))))
-    (build-system mix-build-system)
-    (propagated-inputs
-     (list elixir-ecto
-           elixir-ecto-dev-logger
-           elixir-ecto-sql
-           elixir-html-sanitize-ex
-           elixir-recase
-           elixir-untangle))
-    (synopsis
-     "Helper library to better join + preload Ecto associations")
-    (description
-     "This package provides andhelper library to better join + preload Ecto
-associations, and other goodies.")
-    (home-page "https://hexdocs.pm/ecto_sparkles/")
-    (license license:asl2.0)))
+     (name "bonfire-paginator")
+     (version (git-version version revision commit))
+     (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+              (url "https://github.com/bonfire-networks/paginator.git")
+              (commit commit)))
+        (file-name (git-file-name name version))
+        (sha256
+         (base32 "0xgslf2jis0cnq6z0lq7hcldyiq4ln8ra0b6sf40mqwmf4d02fy0"))))
+     (build-system mix-build-system)
+     (arguments
+      (list
+       ;; Tests require a live Postgres process
+       #:tests? #f
+       #:phases
+       #~(modify-phases %standard-phases
+           (add-after 'unpack 'override-mix-env
+             (lambda _
+               (symlink (string-append (getcwd) "/config/dev.exs")
+                        "config/prod.exs"))))))
+     (native-inputs
+      (list elixir-ex-machina elixir-inch-ex))
+     (propagated-inputs
+      (list elixir-ecto
+            elixir-ecto-sql
+            elixir-needle-uid.git
+            elixir-plug-crypto
+            elixir-untangle))
+     (synopsis
+      "Cursor-based pagination for Elixir Ecto")
+     (description
+      "This package implements cursor-based pagination for Elixir Ecto.")
+     (home-page "https://github.com/bonfire-networks/paginator")
+     (license license:expat))))
 
 (define-public elixir-mess.git
   (let ((version "0.0.0")
         (revision "0")
         (commit "bfe6e3c56c5237017c7fd0fd8d8d804bec267ca6"))
     (package
-      (name "elixir-mess")
-      (version (git-version version revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/bonfire-networks/mess.git")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32 "0ql3lz4y7wfd4cl2dpdr63qh5vrqc6bpf1154yjcgpf947w0784y"))))
-      (build-system mix-build-system)
-      (synopsis
-       "Simple, file-based dependency management with git and local overrides")
-      (description
-       "@code{mess} implements simple, file-based dependency management with git
+     (name "elixir-mess")
+     (version (git-version version revision commit))
+     (source
+      (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/bonfire-networks/mess.git")
+             (commit commit)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0ql3lz4y7wfd4cl2dpdr63qh5vrqc6bpf1154yjcgpf947w0784y"))))
+     (build-system mix-build-system)
+     (synopsis
+      "Simple, file-based dependency management with git and local overrides")
+     (description
+      "@code{mess} implements simple, file-based dependency management with git
 and local overrides.")
-      (home-page "https://github.com/bonfire-networks/mess")
-      (license license:mpl2.0))))
+     (home-page "https://github.com/bonfire-networks/mess")
+     (license license:mpl2.0))))
 
 (define-public elixir-needle-uid.git
   (let ((version "0.0.1")
         (revision "0")
-        (commit "9b3cfcba18322cec0595fa51f90489001381f47b"))
+        (commit "3ec02ce42d96498db286a619a31a40eda2df7fa7"))
     (package
-    (name "elixir-needle-uid")
-    (version (git-version version revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/bonfire-networks/needle_uid.git")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32 "07swqkqvd1p5djylgqs4n4xb71cih7iazzvakfz24s4qwysc5fnc"))))
-    (build-system mix-build-system)
-    (arguments
-     ;; Tests appear to be broken on main
-     ;; 9 doctests, 24 tests, 20 failures
-     (list #:tests? #f))
-    (propagated-inputs
-     (list elixir-ecto
-           elixir-needle-ulid.git
-           elixir-pride.git
-           elixir-untangle))
-    (synopsis
-     "Hybrid prefixed UUIDv7 and ULID data type for Ecto")
-    (description
-     "This package provides hybrid prefixed UUIDv7 and ULID data type for Ecto
+     (name "elixir-needle-uid")
+     (version (git-version version revision commit))
+     (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+              (url "https://github.com/bonfire-networks/needle_uid.git")
+              (commit commit)))
+        (file-name (git-file-name name version))
+        (sha256
+         (base32 "1k5i79cwjy73j47k5q5d4zisfm33w7a9j85431w1rqpvgcv8zzld"))))
+     (build-system mix-build-system)
+     (arguments
+      ;; Tests appear to be broken on main
+      ;; 9 doctests, 24 tests, 20 failures
+      (list #:tests? #f))
+     (propagated-inputs
+      (list elixir-ecto
+            elixir-needle-ulid.git
+            elixir-pride.git
+            elixir-untangle))
+     (synopsis
+      "Hybrid prefixed UUIDv7 and ULID data type for Ecto")
+     (description
+      "This package provides hybrid prefixed UUIDv7 and ULID data type for Ecto
 (using @code{elixir-pride} and @code{elixir-needle-ulid})")
-    (home-page "https://github.com/bonfire-networks/needle_uid")
-    (license license:expat))))
+     (home-page "https://github.com/bonfire-networks/needle_uid")
+     (license license:expat))))
 
 (define-public elixir-pride.git
   (let ((version "0.0.1")
         (revision "0")
-        (commit "82798ea71e703e283b2e9a3e8cdbc80035ad1468"))
+        (commit "0b7f989a9db61d5466ea573f181f08e18086ab72"))
     (package
-    (name "elixir-pride")
-    (version (git-version version revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/bonfire-networks/pride.git")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32 "0y0mx7a972z11zxncbxvifp16inss6d5az8ij26qx4ry5xcrijp8"))))
-    (build-system mix-build-system)
-    (propagated-inputs
-     (list elixir-ecto
-           elixir-ecto-sql
-           elixir-untangle
-           elixir-uuidv7))
-    (synopsis
-     "Prefixed UUIDv7 data type for Ecto")
-    (description
-     "This package provides a prefixed UUIDv7 data type for Ecto, and related
+     (name "elixir-pride")
+     (version (git-version version revision commit))
+     (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+              (url "https://github.com/bonfire-networks/pride.git")
+              (commit commit)))
+        (file-name (git-file-name name version))
+        (sha256
+         (base32 "1pys07c970nm4h7indn46valmp3bi9hvh0x9mdi8cjg6plxhl96c"))))
+     (build-system mix-build-system)
+     (propagated-inputs
+      (list elixir-ecto
+            elixir-ecto-sql
+            elixir-untangle
+            elixir-uuidv7))
+     (synopsis
+      "Prefixed UUIDv7 data type for Ecto")
+     (description
+      "This package provides a prefixed UUIDv7 data type for Ecto, and related
 helpers.")
-    (home-page "https://github.com/bonfire-networks/pride")
-    (license license:expat))))
+     (home-page "https://github.com/bonfire-networks/pride")
+     (license license:expat))))
--- modules/bonfire/packages/elixir-databases.scm
+++ modules/bonfire/packages/elixir-databases.scm
@@ -1,200 +1,42 @@
 ;;; SPDX-License-Identifier: GPL-3.0-or-later
-;;; Copyright © 2024 Giacomo Leidi <goodoldpaul@autistici.org>
+;;; Copyright © 2024, 2025 Giacomo Leidi <goodoldpaul@autistici.org>
 
 (define-module (bonfire packages elixir-databases)
+  #:use-module (gnu packages elixir-databases)
+  #:use-module (gnu packages elixir-markup)
   #:use-module (gnu packages elixir-xyz)
   #:use-module (guix build-system mix)
   #:use-module (guix download)
   #:use-module (guix git-download)
-  #:use-module (guix gexp)
   #:use-module ((guix licenses)
                 #:prefix license:)
-  #:use-module (guix packages)
-  #:use-module (bonfire packages erlang-xyz)
-  #:use-module (bonfire packages elixir-xyz))
+  #:use-module (guix packages))
 
-(define-public elixir-db-connection
+(define-public elixir-ecto-sparkles
   (package
-    (name "elixir-db-connection")
-    (version "2.7.0")
+    (name "elixir-ecto-sparkles")
+    (version "0.2.1")
     (source
      (origin
        (method url-fetch)
-       (uri (hexpm-uri "db_connection" version))
+       (uri (hexpm-uri "ecto_sparkles" version))
        (sha256
-        (base32 "1zykppj97pc1x0apy8ag40r1mmi3hbbvlzvqziyqa7vhn8qqzw6w"))))
-    (build-system mix-build-system)
-    (arguments
-     (list
-      ;; Tests require a db process
-      #:tests? #f
-      #:build-per-environment #f))
-    (propagated-inputs (list erlang-telemetry))
-    (synopsis
-     "Database connection behaviour")
-    (description
-     "This package provides @code{elixir-db-connection}, a library implementing
-database connection behaviour and database connection pool.  It is designed for
-handling transaction, prepare/execute, cursors and client process
-describe/encode/decode.")
-    (home-page "https://hexdocs.pm/db_connection/")
-    (license license:asl2.0)))
-
-(define-public elixir-ecto
-  (package
-    (name "elixir-ecto")
-    (version "3.12.3")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (hexpm-uri "ecto" version))
-       (sha256
-        (base32 "0rcisp34m8b058jrcf52nziyscmn1h6yfjfw91ggj8p7d9893zcy"))))
-    (build-system mix-build-system)
-    (propagated-inputs (list elixir-decimal elixir-jason erlang-telemetry))
-    (synopsis
-     "Toolkit for data mapping and language integrated query for Elixir")
-    (description
-     "This package provides a toolkit for data mapping and language integrated query
-for Elixir.")
-    (home-page "https://hexdocs.pm/ecto/")
-    (license license:asl2.0)))
-
-(define-public elixir-ecto-dev-logger
-  (package
-    (name "elixir-ecto-dev-logger")
-    (version "0.13.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (hexpm-uri "ecto_dev_logger" version))
-       (sha256
-        (base32 "13vml7nj7vxis6gfdr6p5gd4nrdiji1ya2wv6xzn51g9vm2jpv0p"))))
-    (build-system mix-build-system)
-    (native-inputs (list elixir-ecto-sql))
-    (propagated-inputs (list elixir-ecto elixir-geo elixir-jason))
-    (synopsis "Alternative Ecto logger for development")
-    (description "This package provides an alternative Ecto logger for
-development.")
-    (home-page "https://hexdocs.pm/ecto_dev_logger/")
-    (license license:asl2.0)))
-
-(define-public elixir-ecto-sql
-  (package
-    (name "elixir-ecto-sql")
-    (version "3.12.0")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/elixir-ecto/ecto_sql.git")
-             (commit (string-append "v" version))))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "15k7rmf8i6qx25hcqk88j8d6ydd0mf5jalrdyrnlia6ybw99dk7n"))))
+        (base32 "1ry89mk103jpv73971lwx3vmn9n6zd3ycya1ac1pr3r53f4hmkb0"))))
     (build-system mix-build-system)
     (propagated-inputs
-     (list elixir-db-connection
-           elixir-ecto
-           elixir-myxql
-           elixir-postgrex
-           elixir-tds))
-    (synopsis "SQL-based adapters for Ecto and database migrations")
-    (description "@code{Ecto SQL} provides building blocks for writing SQL
-adapters for Ecto.  It features:
-
-@itemize
-@item The @code{Ecto.Adapters.SQL} module as an entry point for all SQL-based
-adapters
-@item Default implementations for Postgres (@code{Ecto.Adapters.Postgres}),
-MySQL (@code{Ecto.Adapters.MyXQL}), and MSSQL (@code{Ecto.Adapters.Tds})
-@item A test sandbox (@code{Ecto.Adapters.SQL.Sandbox}) that concurrently runs
-database tests inside transactions
-@item Support for database migrations via Mix tasks
-@ent itemize")
-    (home-page "https://hexdocs.pm/ecto_sql/")
-    (license license:asl2.0)))
-
-(define-public elixir-ex-machina
-  (package
-    (name "elixir-ex-machina")
-    (version "2.8.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (hexpm-uri "ex_machina" version))
-       (sha256
-        (base32 "0abpg9i08w0iglc0qcl5b3g91jw22s3mvyn4nvxc3hf0cjf1mzkr"))))
-    (build-system mix-build-system)
-    (native-inputs (list elixir-excoveralls))
-    (propagated-inputs (list elixir-ecto elixir-ecto-sql))
+     (list elixir-ecto
+           elixir-ecto-dev-logger
+           elixir-ecto-sql
+           elixir-html-sanitize-ex
+           elixir-recase
+           elixir-untangle))
     (synopsis
-     "Factory library")
+     "Helper library to better join + preload Ecto associations")
     (description
-     "This package provides a factory library by the creators of @code{FactoryBot}
-(@code{FactoryGirl}).")
-    (home-page "https://hexdocs.pm/ex_machina/")
-    (license license:expat)))
-
-(define-public elixir-exto
-  (package
-    (name "elixir-exto")
-    (version "0.4.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (hexpm-uri "exto" version))
-       (sha256
-        (base32 "1mkzxj37dqb966fhqg528wrlfx9ifgxis87np4fqc30rqabgsyj4"))))
-    (build-system mix-build-system)
-    (propagated-inputs (list elixir-accessible elixir-ecto))
-    (native-inputs
-     (list elixir-decimal))
-    (synopsis "Extend ecto schema definitions in config")
-    (description "@code{Exto} is an Elixir library providing
-configuration-driven @code{Ecto} schema extensibility.")
-    (home-page "https://hexdocs.pm/exto/")
+     "This package provides andhelper library to better join + preload Ecto
+associations, and other goodies.")
+    (home-page "https://hexdocs.pm/ecto_sparkles/")
     (license license:asl2.0)))
-
-(define-public elixir-myxql
-  (package
-    (name "elixir-myxql")
-    (version "0.7.1")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (hexpm-uri "myxql" version))
-       (sha256
-        (base32 "0dj9034nfyii8dmd7c1hdkvikgkfh5rd9hhahnshjfimagzwv4d4"))))
-    (build-system mix-build-system)
-    (native-inputs
-     (list erlang-binpp))
-    (propagated-inputs (list elixir-db-connection elixir-decimal elixir-geo elixir-jason
-                  elixir-table))
-    (synopsis "MySQL 5.5+ driver for Elixir")
-    (description "This library provides a @code{MySQL} 5.5+ driver for Elixir.")
-    (home-page "https://hexdocs.pm/myxql/")
-    (license license:asl2.0)))
-
-(define-public elixir-needle-ulid
-  (package
-    (name "elixir-needle-ulid")
-    (version "0.3.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (hexpm-uri "needle_ulid" version))
-       (sha256
-        (base32 "14ryrgsabj08v1a0wyad21aizyy8kcr0n1l1z6ks6fvv4v2hskfm"))))
-    (build-system mix-build-system)
-    (propagated-inputs (list elixir-ecto elixir-ecto-sql elixir-ex-ulid))
-    (synopsis
-     "Provides an ULID datatype for @code{Ecto} and related helpers")
-    (description
-     "This package provides @code{elixir-needle-ulid}, a library implementing an
-ULID datatype for @code{Ecto} (using @code{ex_ulid}) and related helpers.")
-    (home-page "https://hexdocs.pm/needle_ulid/")
-    (license license:expat)))
 
 (define-public elixir-needle-ulid.git
   (let ((version (package-version elixir-needle-ulid))
@@ -213,127 +55,3 @@ ULID datatype for @code{Ecto} (using @code{ex_ulid}) and related helpers.")
          (file-name (git-file-name name version))
          (sha256
           (base32 "1iknjj3jhj5z2wyjym8bvz6syrwjx6wvmbc246a9rm25yd0szn44")))))))
-
-(define-public elixir-needle
-  (package
-    (name "elixir-needle")
-    (version "0.8.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (hexpm-uri "needle" version))
-       (sha256
-        (base32 "0gf1bniwf650xrczvimmw1drg6cjqz21539nlhqw4la4m91jl7h4"))))
-    (build-system mix-build-system)
-    (propagated-inputs (list elixir-ecto-sql elixir-exto elixir-needle-ulid
-                             erlang-telemetry))
-    (synopsis
-     "Universal foreign keys, virtual schemas, and shared data mixins")
-    (description
-     "When using a relational DB (such an @code{Ecto} schema with a Postgres
-table), usually a foreign key field has to be pre-defined with a reference
-pointing to a specific field in a specific table.
-
-Besides regular schemas with universal foreign keys (@code{Pointable}),
-@code{Needle} provides @code{Virtual} for schemas that don’t have any fields on
-their own, and @code{Mixin} for storing common fields that can be re-used by
-multiple @code{Pointable}s or @code{Virtual}s.")
-    (home-page "https://hexdocs.pm/needle/")
-    (license license:asl2.0)))
-
-(define-public elixir-postgrex
-  (package
-    (name "elixir-postgrex")
-    (version "0.19.1")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (hexpm-uri "postgrex" version))
-       (sha256
-        (base32 "1y58v3jsya98462r43mv29wwgf3vv8dz9jn63q4iwf4gl62pib4b"))))
-    (build-system mix-build-system)
-    (propagated-inputs (list elixir-db-connection elixir-decimal elixir-jason
-                  elixir-table))
-    (synopsis "PostgreSQL driver for Elixir")
-    (description "This library provides a @code{PostgreSQL} driver for Elixir.")
-    (home-page "https://hexdocs.pm/postgrex/")
-    (license license:asl2.0)))
-
-(define-public elixir-table
-  (package
-    (name "elixir-table")
-    (version "0.1.2")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (hexpm-uri "table" version))
-       (sha256
-        (base32 "0aah0rkq6ikl1w359n65rmhk0p0npwj0fr35grf321pqzrzbr6by"))))
-    (build-system mix-build-system)
-    (synopsis "Unified access to tabular data")
-    (description "This package provides @code{elixir-table}, a library that
-implements unified access to tabular data.")
-    (home-page "https://hexdocs.pm/table/")
-    (license license:asl2.0)))
-
-(define-public elixir-tds
-  (package
-    (name "elixir-tds")
-    (version "2.3.5")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (hexpm-uri "tds" version))
-       (sha256
-        (base32 "1wk20rk1fjh16swlkiyl3fs91lj4w4dk77l5z77vp12mvpsm1qsj"))))
-    (build-system mix-build-system)
-    (propagated-inputs
-     (list elixir-db-connection elixir-decimal elixir-jason
-           elixir-table))
-    (synopsis
-     "Elixir implementation of the MS TDS protocol")
-    (description
-     "This package implements a Microsoft SQL Server client.  More generally it
-provides an Elixir implementation of the MS TDS protocol.")
-    (home-page "https://hexdocs.pm/tds/")
-    (license license:asl2.0)))
-
-(define-public elixir-uniq
-  (package
-    (name "elixir-uniq")
-    (version "0.6.1")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (hexpm-uri "uniq" version))
-       (sha256
-        (base32 "05mrzx7g8q5c4jg38pxqcw1x3bysw0i5n4j7d42v6m3hcx6w69k4"))))
-    (build-system mix-build-system)
-    (native-inputs (list elixir-stream-data))
-    (propagated-inputs (list elixir-ecto))
-    (synopsis
-     "Provides UUID generation, parsing, and formatting")
-    (description
-     "This package provides UUID generation, parsing, and formatting.  Supports RFC
-4122, and the v6 draft extension.")
-    (home-page "https://hexdocs.pm/uniq/")
-    (license license:asl2.0)))
-
-(define-public elixir-uuidv7
-  (package
-    (name "elixir-uuidv7")
-    (version "1.0.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (hexpm-uri "uuidv7" version))
-       (sha256
-        (base32 "1g2c8imkk7pgcvc5ns2v1b0zils3ai1yym4s3a5psvs511qk7k8f"))))
-    (build-system mix-build-system)
-    (propagated-inputs (list elixir-ecto))
-    (synopsis "UUID v7 implementation and @code{Ecto.Type} for Elixir")
-    (description
-     "This package provides a UUID v7 implementation and @code{Ecto.Type}
-for Elixir.")
-    (home-page "https://hexdocs.pm/uuidv7/")
-    (license license:expat)))
